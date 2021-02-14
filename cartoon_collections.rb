7058fc591b2bfa3bbe5a989b71c91626d77d74f1
Mathew E Newton <elijah2895@hotmail.com>
2021-02-14 00:49:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ def long_planeteer_calls(planeteer_calls)
   # Return the boolean result of this check
   planeteer_calls.any? do |element|
     if element.length >= 4
-      element = true
+      element == true
     else
       element.length <= 4
       element == false
